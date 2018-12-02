020d84673fbcc43194c0109b8539422b9ea519c1
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2018-12-02 18:56:51 +0100

    Added docker handling in debug (exposinig port 5000).

--- services/DataCollector/Program.cs
+++ services/DataCollector/Program.cs
@@ -19,7 +19,7 @@ namespace datacollector
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:5000", "http://192.168.1.107:5000")
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>();
     }
 }
