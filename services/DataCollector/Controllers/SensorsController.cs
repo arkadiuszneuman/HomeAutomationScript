3c393acf19d49d3f5bf80169d21ee522bd016e48
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2018-12-02 18:11:49 +0100

    Added simple DataCollector

---
+++ services/DataCollector/Controllers/SensorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datacollector.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorsController : ControllerBase
+    {
+        [HttpGet("StairsSensorDown")]
+        public ActionResult<string> StairsSensorDown()
+        {
+            return Ok("Got sensor down info");
+        }
+
+        [HttpGet("StairsSensorUp")]
+        public ActionResult<string> StairsSensorUp()
+        {
+            return Ok("Got sensor up info");
+        }
+    }
+}
