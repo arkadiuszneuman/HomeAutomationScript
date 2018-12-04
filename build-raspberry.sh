eaacd7027b58f0fc40efb7e8d9a6d1a911a6a1a4
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2018-12-04 17:50:44 +0100

    Fixed bash scripts.

--- build-raspberry.sh
+++ build-raspberry.sh
@@ -1,7 +1,6 @@
  echo "=====================Starting========================"
- docker-compose -f ./docker-compose.yaml build
  docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
- docker-compose -f ./docker-compose.yaml -f up
+ docker-compose -f ./docker-compose.yaml up --build
  echo ""=====================Stopping========================""
  docker stop homeassistant_rabbitmq_1
  docker stop homeassistant_mosquitto_1
--- build-windows.sh
+++ build-windows.sh
@@ -1,7 +1,6 @@
- echo "=====================Starting========================"
- docker-compose -f ./docker-compose.yaml build
+ echo "=====================Starting========================"``
  docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
- docker-compose -f ./docker-compose.yaml -f ./docker-compose.windows.yaml up
+ docker-compose -f ./docker-compose.yaml -f ./docker-compose.windows.yaml up --build
  echo ""=====================Stopping========================""
  docker stop homeassistant_rabbitmq_1
  docker stop homeassistant_mosquitto_1
