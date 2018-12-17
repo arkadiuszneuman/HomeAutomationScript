6da49bae767bafa6b4df54209087715070d8c18c
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2018-12-17 19:47:12 +0100

    Named containers.

--- build-windows.sh
+++ build-windows.sh
@@ -2,6 +2,6 @@
  docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
  docker-compose -f ./docker-compose.yaml -f ./docker-compose.windows.yaml up --build
  echo ""=====================Stopping========================""
- docker stop homeautomationscript_mosquitto_1
- docker stop homeautomationscript_influxdb_1
- docker stop homeautomationscript_rabbitmq_1
+ docker stop mosquitto
+ docker stop influxdb
+ docker stop rabbit
