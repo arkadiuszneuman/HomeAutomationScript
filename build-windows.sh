8ac9c5fcc0027c97749c68f2428c137ee0ae2a39
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-01-08 12:36:41 +0100

    Some scrips fixes.

--- build-windows.sh
+++ build-windows.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
  echo "=====================Starting========================"``
  docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
  docker-compose -f ./docker-compose.yaml -f ./docker-compose.windows.yaml up --build
