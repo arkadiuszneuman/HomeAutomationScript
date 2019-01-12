941e7a919a97841e0cdda07acb349172a0d91730
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-01-12 13:23:20 +0100

    Created backup creator project.

---
+++ services/Services.BackupCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Services.BackupCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
