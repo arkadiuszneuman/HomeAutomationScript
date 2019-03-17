3fc66708785a712a6fe6b55253729dddcbfc10f8
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-03-17 13:59:16 +0100

    Update scripts for rpi

---
+++ build-all.rpi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ echo "=====================Starting ELK stack========================"``
+ docker-compose -f ./docker-compose-elk.rpi.yaml up -d
+ echo "=====================Starting========================"
+ docker-compose -f ./docker-compose.dependencies.yaml up
+ docker-compose -f ./docker-compose.yaml up --build
--- build-raspberry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- echo "=====================Starting========================"
- docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
- docker-compose -f ./docker-compose.yaml up --build
- echo "=====================Stopping========================"
- docker stop mosquitto
- docker stop influxdb
- docker stop rabbit
- 
