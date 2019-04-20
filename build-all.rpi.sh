c034f9e00f548881ea2d027d75e24aed1e9684dd
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-04-20 11:40:34 +0200

    Fix rpi script

--- build-all.rpi.sh
+++ build-all.rpi.sh
@@ -2,5 +2,5 @@
  echo "=====================Starting ELK stack========================"``
  docker-compose -f ./docker-compose-elk.rpi.yaml up -d
  echo "=====================Starting========================"
- docker-compose -f ./docker-compose.dependencies.yaml up
+ docker-compose -f ./docker-compose.dependencies.yaml up -d
  docker-compose -f ./docker-compose.yaml up --build
