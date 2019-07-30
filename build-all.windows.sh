e335404b1c930fe966b9e2dc044751d2285b7774
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-07-30 22:34:46 +0200

    Fix elasticsearch dir

--- build-all.windows.sh
+++ build-all.windows.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
  echo "=====================Starting ELK stack========================"``
+ [ -d elasticsearchdata ] || mkdir elasticsearchdata
+ chmod g+rwx elasticsearchdata
+ chgrp 1000 elasticsearchdata
  docker-compose -f ./docker-compose-elk.windows.yaml up -d
  echo "=====================Starting========================"
  docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
--- build-elk.windows.sh
+++ build-elk.windows.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
  echo "=====================Starting ELK stack========================"``
+ [ -d elasticsearchdata ] || mkdir elasticsearchdata
+ chmod g+rwx elasticsearchdata
+ chgrp 1000 elasticsearchdata
  docker-compose -f ./docker-compose-elk.windows.yaml up
