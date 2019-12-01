a5b0784d27aa77d97fb464a6ca6bb4a17bebf125
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-12-01 14:58:16 +0100

    Move HomeAssistantHttpClient to external factory

--- services/AutomationRunner/AutomationRunner/DaemonService.cs
+++ services/AutomationRunner/AutomationRunner/DaemonService.cs
@@ -1,4 +1,5 @@
-﻿using AutomationRunner.Config;
+﻿using AutomationRunner.Common;
+using AutomationRunner.Config;
 using AutomationRunner.Secrets;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,33 +13,19 @@ namespace AutomationRunner
 {
     public class DaemonService : IHostedService
     {
-        private readonly HomeAssistantConfiguration homeAssistantConfiguration;
-        private readonly SecretsConfig secretsConfig;
+        private readonly HomeAssistantHttpClientFactory httpClientFactory;
 
-        public DaemonService(HomeAssistantConfiguration homeAssistantConfiguration,
-            SecretsConfig secretsConfig)
+        public DaemonService(HomeAssistantHttpClientFactory httpClientFactory)
         {
-            this.homeAssistantConfiguration = homeAssistantConfiguration;
-            this.secretsConfig = secretsConfig;
+            this.httpClientFactory = httpClientFactory;
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            var handler = new HttpClientHandler();
-            handler.ServerCertificateCustomValidationCallback +=
-                (sender, cert, chain, sslPolicyErrors) => true;
-
-            var client = new HttpClient(handler);
-            client.BaseAddress = new Uri(homeAssistantConfiguration.Uri);
-            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {secretsConfig.HomeAssistantToken}");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-            var xxx = client.GetAsync("api/").Result.Content.ReadAsStringAsync().Result;
+            using var client = httpClientFactory.GetHomeAssistantHttpClient();
+            var xxx = await (await client.GetAsync("api/events")).Content.ReadAsStringAsync();
 
             Console.WriteLine(xxx);
-
-            return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
---
+++ services/AutomationRunner/AutomationRunner/Modules/CommonModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+namespace AutomationRunner.Modules
+{
+    public class CommonModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(a => a.Namespace == "AutomationRunner.Common")
+                .AsImplementedInterfaces()
+                .AsSelf();
+        }
+    }
+}
