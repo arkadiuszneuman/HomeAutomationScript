35acab9a8ee93b11b5b59deca43321f6c4627825
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-12-14 23:22:28 +0100

    Add websocket listener

--- services/AutomationRunner/AutomationRunner/Config/HomeAssistantConfiguration.cs
+++ services/AutomationRunner/AutomationRunner/Config/HomeAssistantConfiguration.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AutomationRunner.Config
+﻿namespace AutomationRunner.Config
 {
     public class HomeAssistantConfiguration
     {
         public string Uri { get; set; }
+        public string WebSocketsUri { get; set; }
     }
 }
