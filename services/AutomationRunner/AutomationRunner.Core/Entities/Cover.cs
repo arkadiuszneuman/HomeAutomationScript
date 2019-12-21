9de244751289cae8b086863918e3714a34f508f8
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-12-21 23:50:05 +0100

    Add cover and device tracker

---
+++ services/AutomationRunner/AutomationRunner.Core/Entities/Cover.cs
@@ -0,0 +1,32 @@
+﻿using AutomationRunner.Core.Common;
+using AutomationRunner.Core.Common.Connector;
+using AutomationRunner.Core.Common.Extensions;
+using AutomationRunner.Core.Entities.Services.Models;
+using System.Threading.Tasks;
+
+namespace AutomationRunner.Core.Entities
+{
+    public class Cover : BaseEntity
+    {
+        public enum Name
+        {
+            [EntityId("cover.roleta_salon")]
+            Salon
+        }
+
+        public static async Task<Cover> LoadFromEntityId(HomeAssistantConnector entityLoader, Name coverName)
+        {
+            return await entityLoader.LoadEntityFromStates<Cover>(coverName.GetEntityId());
+        }
+
+        public async Task OpenCover()
+        {
+            await Connector.SendService("cover.open_cover", new EntityIdService(EntityId));
+        }
+
+        public async Task CloseCover()
+        {
+            await Connector.SendService("cover.close_cover", new EntityIdService(EntityId));
+        }
+    }
+}
---
+++ services/AutomationRunner/AutomationRunner.Core/Entities/DeviceTracker.cs
@@ -0,0 +1,56 @@
+﻿using AutomationRunner.Core.Common;
+using AutomationRunner.Core.Common.Connector;
+using AutomationRunner.Core.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomationRunner.Core.Entities
+{
+    public class DeviceTracker : BaseEntity
+    {
+        public enum Name
+        {
+            [EntityId("device_tracker.life360_arek")]
+            Arek,
+
+            [EntityId("device_tracker.life360_patrycja")]
+            Patrycja
+        }
+
+        public int BatteryLevel => GetAttributeValue<int>("battery");
+        public bool BatteryCharging => GetAttributeValue<bool>("battery_charging");
+        public decimal Latitude => GetAttributeValue<decimal>("latitude");
+        public decimal Longitude => GetAttributeValue<decimal>("longitude");
+
+
+        public static async Task<DeviceTracker> LoadFromEntityId(HomeAssistantConnector connector, Name deviceTrackerName)
+        {
+            return await connector.LoadEntityFromStates<DeviceTracker>(deviceTrackerName.GetEntityId());
+        }
+
+        public static IEnumerable<Task<DeviceTracker>> LoadFromEntitiesId(HomeAssistantConnector connector, params Name[] deviceTrackerNames)
+        {
+            foreach (var lightName in deviceTrackerNames)
+                yield return LoadFromEntityId(connector, lightName);
+        }
+
+        public static IEnumerable<Task<DeviceTracker>> LoadAllLights(HomeAssistantConnector connector, params Name[] except)
+        {
+            foreach (Name lightName in ((Name[])Enum.GetValues(typeof(Name))).Except(except))
+                yield return LoadFromEntityId(connector, lightName);
+        }
+
+        public static async Task<bool> IsAnyoneHome(HomeAssistantConnector connector)
+        {
+            var deviceTrackersTasks = LoadFromEntitiesId(connector, Name.Arek, Name.Patrycja);
+
+            var deviceTrackers = new List<DeviceTracker>();
+            foreach (var deviceTrackerTask in deviceTrackersTasks)
+                deviceTrackers.Add(await deviceTrackerTask);
+
+            return deviceTrackers.Any(t => t.State == "home");
+        }
+    }
+}
