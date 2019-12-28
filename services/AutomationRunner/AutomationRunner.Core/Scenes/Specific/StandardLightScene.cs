2c508be79473d0c4246c280bdbf4009553b1f6b6
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-12-28 22:07:24 +0100

    Add tv leds to standard light

--- services/AutomationRunner/AutomationRunner.Core/Scenes/Specific/StandardLightScene.cs
+++ services/AutomationRunner/AutomationRunner.Core/Scenes/Specific/StandardLightScene.cs
@@ -23,11 +23,13 @@ namespace AutomationRunner.Core.Scenes.Specific
         public async Task Activated(CancellationToken cancellationToken = default)
         {
             var mushroom = await RgbLight.LoadFromEntityId(connector, RgbLight.Name.Mushroom);
+            var tvleds = await RgbLight.LoadFromEntityId(connector, RgbLight.Name.TvLEDs);
             var stairs = await InputNumber.LoadFromEntityId(connector, InputNumber.Name.StairsMinimumBrightness);
 
             var switches = Switch.LoadFromEntitiesId(connector, Switch.Name.ChildLight, 
                 Switch.Name.ChristmasTree, Switch.Name.SalonLights);
 
+            await tvleds.TurnOn(brightnessPercent: 50);
             await switches.TurnOnAll();
             await stairs.SetValueBasedOnTvState();
             await mushroom.TurnOnWithRandomColor();
