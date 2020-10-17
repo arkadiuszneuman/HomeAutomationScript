a6830ee883a51c19ea671f89f8189b7a85a07c17
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2020-10-17 14:03:39 +0200

    Update Bravia LED automation

--- services/AutomationRunner/AutomationRunner.Core/Automations/Specific/Bravia/BraviaSwitchOnLedLightsAutomation.cs
+++ services/AutomationRunner/AutomationRunner.Core/Automations/Specific/Bravia/BraviaSwitchOnLedLightsAutomation.cs
@@ -6,13 +6,13 @@ using AutomationRunner.Core.Common.Connector;
 using AutomationRunner.Core.Common.Extensions;
 using AutomationRunner.Core.Entities;
 
-namespace AutomationRunner.Core.Automations.Specific.Fan.Bravia
+namespace AutomationRunner.Core.Automations.Specific.Bravia
 {
     public class BraviaSwitchOnLedLightsAutomation : IEntitiesStateAutomation
     {
         private readonly HomeAssistantConnector connector;
         private readonly IDateTimeHelper dateTimeHelper;
-        public string EntityName => MediaPlayer.Name.Tv.GetEntityId();
+
         private DateTime? lastUpdateAfterSunset;
 
         public IEnumerable<string> EntityNames =>
@@ -21,8 +21,6 @@ namespace AutomationRunner.Core.Automations.Specific.Fan.Bravia
                 MediaPlayer.Name.Tv.GetEntityId(),
                 Sun.EntityId
             };
-
-        public TimeSpan UpdateEvery => TimeSpan.FromMinutes(5);
 
         public BraviaSwitchOnLedLightsAutomation(HomeAssistantConnector connector,
             IDateTimeHelper dateTimeHelper)
@@ -33,33 +31,35 @@ namespace AutomationRunner.Core.Automations.Specific.Fan.Bravia
 
         public async Task Update(BaseEntity oldStateBaseEntity, BaseEntity newStateBaseEntity)
         {
-            var mushroom = await RgbLight.LoadFromEntityId(connector, RgbLight.Name.Mushroom);
-            var tvleds = await RgbLight.LoadFromEntityId(connector, RgbLight.Name.TvLEDs);
-            
             if (oldStateBaseEntity.EntityId == MediaPlayer.Name.Tv.GetEntityId())
             {
                 var oldState = MediaPlayer.CreateBasedOnBaseEntity(oldStateBaseEntity);
                 var newState = MediaPlayer.CreateBasedOnBaseEntity(newStateBaseEntity);
 
                 if (oldState.State == "off" && newState.State == "on")
-                {
-                    await tvleds.TurnOnStandardWhite();
-                    await mushroom.TurnOnWithRandomColor();
-                }
+                    await TurnOn();
             }
             else if (oldStateBaseEntity.EntityId == Sun.EntityId)
             {
-                if (lastUpdateAfterSunset == null || 
+                if (lastUpdateAfterSunset == null ||
                     lastUpdateAfterSunset.Value.Date != DateTime.UtcNow.Date)
-                {
-                    var sun = await Sun.Load(connector, dateTimeHelper);
-                    if (sun.AfterSunset(-TimeSpan.FromMinutes(30)))
-                    {
-                        lastUpdateAfterSunset = DateTime.UtcNow;
-                        await tvleds.TurnOnStandardWhite();
-                        await mushroom.TurnOnWithRandomColor();
-                    }
-                }
+                    await TurnOn();
+            }
+        }
+
+        private async Task TurnOn()
+        {
+            var mushroom = await RgbLight.LoadFromEntityId(connector, RgbLight.Name.Mushroom);
+            var tvleds = await RgbLight.LoadFromEntityId(connector, RgbLight.Name.TvLEDs);
+            var sun = await Sun.Load(connector, dateTimeHelper);
+            var tv = await MediaPlayer.LoadFromEntityId(connector, MediaPlayer.Name.Tv);
+
+            if (tv.State == "on" &&
+                sun.AfterSunset(-TimeSpan.FromMinutes(30)))
+            {
+                lastUpdateAfterSunset = DateTime.UtcNow;
+                await tvleds.TurnOnStandardWhite();
+                await mushroom.TurnOnWithRandomColor();
             }
         }
     }
