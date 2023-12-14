6c706bf3659d455b0bb730625f32f976ebc06ab5
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2023-12-14 10:24:24 +0100

    update

--- build-all.windows.sh
+++ build-all.windows.sh
@@ -6,4 +6,4 @@
  docker-compose -f ./docker-compose-elk.windows.yaml up -d
  echo "=====================Starting========================"
  docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
- docker-compose -f ./docker-compose.yaml -f ./docker-compose.windows.yaml up --build
+ docker-compose -f ./docker-compose.windows.yaml up
