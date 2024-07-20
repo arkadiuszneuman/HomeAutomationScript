7f09b3bfa70657ecc7054ad3dd4698048ecb8705
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2024-07-20 10:10:29 +0000

    update

--- build-all.windows.sh
+++ build-all.windows.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
- echo "=====================Starting ELK stack========================"``
- [ -d elasticsearchdata ] || mkdir elasticsearchdata
- chmod g+rwx elasticsearchdata
- chgrp 1000 elasticsearchdata
- docker-compose -f ./docker-compose-elk.windows.yaml up -d
+# echo "=====================Starting ELK stack========================"``
+# [ -d elasticsearchdata ] || mkdir elasticsearchdata
+# chmod g+rwx elasticsearchdata
+# chgrp 1000 elasticsearchdata
+# docker-compose -f ./docker-compose-elk.windows.yaml up -d
  echo "=====================Starting========================"
  docker-compose -f ./docker-compose.dependencies.yaml run --rm start_dependencies
  docker-compose -f ./docker-compose.windows.yaml up
